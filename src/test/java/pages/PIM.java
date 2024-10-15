fd6fb0b758af268ff49d6c90aee79ab9a3fcae32
igawad138 <igawad138@outlook.com>
2024-10-15 21:43:09 +0300

    remove added class PIM

--- src/test/java/pages/PIM.java
+++ src/test/java/pages/PIM.java
@@ -5,9 +5,5 @@ import org.openqa.selenium.WebDriver;
 
 public class PIM extends PageBase {
 
-    public PIM(WebDriver driver){
-        super(driver);
-    }
-    private By ADDButton = By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button");
 
 }
